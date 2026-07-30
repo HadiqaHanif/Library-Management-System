97a1c4c5ae6b22deb4c4eecd78c56accc7751457
Hadiqa Hanif <hadiqaa201@gmail.com>
2026-07-30 16:02:40 +0500

    Add files via upload

---
+++ LibraryManagementSystem.py
@@ -0,0 +1,101 @@
+# For Clear Output
+import os 
+os. system ("cls")
+# Defining Class
+class BookLibrary:
+    Library_books = ["A", "B", "C", "D"]
+    book_borrow = False # book is available 
+    # User Registration Method
+    def user_data(self):
+        self.name = input("Enter your name: ")
+        while True:
+            try:
+                self.age = int(input("Enter your age: "))
+                break
+            except ValueError:
+                print("Invalid Input. Please try again with a number.")
+        while True:
+            try:
+                self.grade = int(input("Enter your grade: "))
+                break
+            except ValueError:
+                print("Invalid Input. Try again !")
+        print(f"{self.name} of age {self.age} years and {self.grade} grade is successfully registered to Library Management System.")
+    # Allowing Users to Borrow Book
+    def borrow_book(self):
+        self.name = input("Enter your name: ")
+        self.book_name = input("Enter Book Name: ").upper()
+        if self.book_name in BookLibrary.Library_books :
+            print("This book is available. Type borrow to borrow it.")
+            user_input = input("Type Here: ").lower()
+            if user_input == "borrow" :
+                print(f"Book is succesfully borrowed by {self.name}.")
+                BookLibrary.book_borrow = True # book is borrowed now 
+                BookLibrary.Library_books.remove(self.book_name)
+            else:
+                print("Try Again with a valid response.")
+        else:
+            print("Book is not available yet. Try again later.")
+    # Allowing Users to return borrowed book
+    def return_book(self):
+        self.name_of_return_book = input("Enter Book Name: ")
+        BookLibrary.Library_books.append(self.name_of_return_book)
+        print("Book is returned successfully! Thankyou <3 ")
+    # Allowing Librarian to Add New Books
+    def add_new_book(self):
+        self.new_book = input("Enter Book Name: ")
+        BookLibrary.Library_books.append(self.new_book)
+        BookLibrary.Library_books.sort()
+        print("Book Library after adding new book: ", BookLibrary.Library_books )
+    # Allowing Users to look our books!
+    def view_books_menu(self):
+        print("Select a book for you!")
+        print("Here's our book menu: ", BookLibrary.Library_books)
+    # Allowing Users to select what they want.
+    def selection_to_do(self):
+        while True:
+            print("--" * 7 ,"Library Management System",  "--" * 7)
+            print("Please select an option to continue: ")
+            print("0. Exit")
+            print("1. Register")
+            print("2. View Our Book Menu")
+            print("3. Borrow Book")
+            print("4. Return Book")
+            print("5. Add New Book")
+            while True:
+                try:
+                    self.choice = int(input("Enter your choice: "))
+                    break
+                except ValueError:
+                    print("Please Choose Option Between 0 and 4")
+            if self.choice == 1 :
+                self.user_data()
+            elif self.choice == 2 :
+                self.view_books_menu()
+            elif self.choice == 3 :
+                self.borrow_book()
+            elif self.choice == 0:
+                print("Are you sure to exit?\n" , "(Yes/No)")
+                a = input("Your choice here: ").lower()
+                if a == "no":
+                    continue
+                else:
+                    print("Exited")
+                    break
+            elif self.choice == 4 :
+                self.return_book()
+            elif self.choice == 5:
+                self.add_new_book()
+            else:
+                print("Option not available yet.")
+            while True:
+                print("Press 'E' to exit and 'M' to return to options dashboard : ")
+                b = input("Enter your choice: ").upper()
+                if b == "E":
+                    exit()
+                elif b == "M":
+                    break
+                else:
+                    print("Invalid Input! Select only 'E' or 'M'.")
+user = BookLibrary()
+user.selection_to_do()
